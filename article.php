ea127f6d45074f4a99c6b6d664fd9ead0adb99a4
Nicolas Chambrier <naholyr@gmail.com>
2011-04-11 16:16:19 +0200

    Fixed paths JS/CSS

--- article.php
+++ article.php
@@ -25,9 +25,9 @@ if (!$article) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
 	<title>Article - Titre</title>
-	<link rel="stylesheet"  href="http://code.jquery.com/mobile/1.0a4/jquery.mobile-1.0a4.min.css" />
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
-	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a4/jquery.mobile-1.0a4.min.js"></script>
+	<script type="text/javascript" src="jquery.mobile-1.0beta-pre.min.js"></script>
+	<link rel="stylesheet"  href="jquery.mobile-1.0beta-pre.min.css" />
 </head>
 
 <body>
